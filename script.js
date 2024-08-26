386808d48471ad25efc80c4d66a8d2c6265ec388
mikegood <119064359+MIKEGOOD-ETH@users.noreply.github.com>
2024-08-25 21:50:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('search-input').addEventListener('input', filterImages);
+document.getElementById('filter').addEventListener('change', filterImages);
+
+function filterImages() {
+    const keyword = document.getElementById('search-input').value.toLowerCase();
+    const filter = document.getElementById('filter').value;
+    const images = document.querySelectorAll('.grid-item');
+
+    images.forEach(image => {
+        const tags = image.getAttribute('data-tags').toLowerCase().split(', ');
+        const title = image.querySelector('h3').textContent.toLowerCase();
+        const description = image.querySelector('p').textContent.toLowerCase();
+
+        const matchesFilter = filter === 'all' || tags.includes(filter);
+        const matchesKeyword = keyword === '' || tags.some(tag => tag.includes(keyword)) || title.includes(keyword) || description.includes(keyword);
+
+        if (matchesFilter && matchesKeyword) {
+            image.style.display = 'flex';
+        } else {
+            image.style.display = 'none';
+        }
+    });
+}
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+}
